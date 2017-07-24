72c3eea89bdaf02412cf661adb5919a26340714c
Jason Coman <jasoncoman@Jasons-iMac.home>
2017-07-24 12:26:05 +0100

    Added H1 heading

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component {
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
+          <h1>Test 2</h1>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to re-load.
