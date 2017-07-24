f72a2628155e7573a45f7860833b72f87958a4da
Jason Coman <jasoncoman@Jasons-iMac.home>
2017-07-24 13:06:10 +0100

    Added top image, bootstrap css, new solarcss file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import solarimg from './solar4.jpg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="container">
+            <div className="solarimage">
+                  <img src={solarimg} className="solarimg" alt="solar panel logo" width="100%" />
+            </div>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to re-load.
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import './bootstrap.css';
 import './index.css';
+import './mysolar.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
